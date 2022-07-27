9902ffc13aa1650ba878ad1483f44683dc0cbd7e
Kabilash <704730@pdsb.net>
2022-07-27 13:07:00 -0400

    Initial Commit

---
+++ Weekendify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weekendify
+//
+//  Created by Student on 2022-07-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
